9f8ca637a68f2b21116593defddb7fa5156ae7d2
Tito S <150861347+HectorSchulz@users.noreply.github.com>
2024-06-30 16:25:07 -0400

    Add files via upload

---
+++ php/conexion_be.php
@@ -0,0 +1,11 @@
+<?php
+
+    $conexion = mysqli_connect("localhost","root","","log_reg_db");
+    /*
+    if($conexion){
+        echo 'Conectando a la DataBase';
+    }else{
+        echo 'No se ha podido conectar la DataBase';
+    }
+    */
+?>
